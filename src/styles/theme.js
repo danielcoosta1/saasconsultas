65f3d1c49f66575bc1639e0ee10d93054a003b4a
Daniel Costa <daniel.dc697@gmail.com>
2025-06-05 08:07:53 -0300

    Adicionei a cor de fundo dos inputs(padrão)

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,19 +1,20 @@
 // src/styles/theme.js
 export const theme = {
   colors: {
-    primary: '#536DFE',    // Cor principal para botões, ícones e detalhes
-    secondary: '#FF7043',  // Usada de forma discreta para destaques
-    background: '#F5F5F5', // Cor de fundo principal
-    text: '#212121',       // Cor padrão para textos
+    primary: "#536DFE", // Cor principal para botões, ícones e detalhes
+    secondary: "#FF7043", // Usada de forma discreta para destaques
+    background: "#F5F5F5", // Cor de fundo principal
+    text: "#212121",
+    inputs: "#F7F7F8", // Cor padrão para textos
   },
   fonts: {
     heading: '"Montserrat", sans-serif',
     body: '"Roboto", sans-serif',
   },
   spacing: {
-    small: '8px',
-    medium: '16px',
-    large: '24px',
+    small: "8px",
+    medium: "16px",
+    large: "24px",
   },
   // Outros tokens de design podem ser adicionados aqui.
-}; 
+};
