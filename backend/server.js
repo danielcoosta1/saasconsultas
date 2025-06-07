76a51fe79c01118fe5fa04308a1841968c343201
Daniel Costa <daniel.dc697@gmail.com>
2025-06-07 11:55:03 -0300

    Importando o dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config(); // Carrega as variáveis do arquivo .env
+
 import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
