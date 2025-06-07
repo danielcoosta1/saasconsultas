acd9955b6f3103a979135c72ff542f074e6516ef
Daniel Costa <daniel.dc697@gmail.com>
2025-06-07 11:12:00 -0300

    Adicionando a variável VITE_API_URL no .env do fronto; ✔ Facilita troca entre ambiente local e produção.

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
+/* global process */
 
 const app = express();
 const prisma = new PrismaClient();
 
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+prisma.$connect()
+  .then(() => console.log('Conexão com o banco de dados estabelecida.'))
+  .catch((error) => {
+    console.error('Erro ao conectar com o banco de dados:', error);
+    process.exit(1);
+  });
 
 // Teste de conexão
 app.get("/", (req, res) => {
   res.json({ message: "Servidor rodando 🚀" });
 });
 
-app.listen(5000, () => {
-  console.log("Servidor rodando em http://localhost:5000");
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
+
