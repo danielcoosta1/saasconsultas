50d9d8df395e8d78112acf17cb325aae68172560
Daniel Costa <daniel.dc697@gmail.com>
2025-06-07 12:49:29 -0300

    Adicionando o AuthProvider no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,40 @@ import Analytics from "./pages/Analytics";
 import Schedule from "./pages/Schedule";
 import Calendar from "./pages/Calendar";
 
-
 import Notifications from "./pages/Notifications";
-import Settings from "./pages/Settings"
+import Settings from "./pages/Settings";
 import Profile from "./pages/Profile";
 
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/auth/AuthProvider";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        {/* Rotas Públicas */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/recover" element={<Recover />} />
+      <AuthProvider>
+        <Routes>
+          {/* Rotas Públicas */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/recover" element={<Recover />} />
 
-        {/* Rotas Protegidas com o layout de menu lateral */}
-        <Route path="/" element={<DashboardLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="analytics" element={<Analytics />} />
-          <Route path="Schedule" element={<Schedule />} />
-          <Route path="calendar" element={<Calendar />} />
+          {/* Rotas Protegidas com o layout de menu lateral */}
+          <Route path="/" element={<DashboardLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="analytics" element={<Analytics />} />
+            <Route path="Schedule" element={<Schedule />} />
+            <Route path="calendar" element={<Calendar />} />
 
-          {/* Rotas adicionais conforme o roadmap */}
-          <Route path="notifications" element={<Notifications />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
+            {/* Rotas adicionais conforme o roadmap */}
+            <Route path="notifications" element={<Notifications />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
 
-        {/* Rota para páginas não encontradas */}
-        <Route path="*" element={<h1>Página não encontrada</h1>} />
-      </Routes>
+          {/* Rota para páginas não encontradas */}
+          <Route path="*" element={<h1>Página não encontrada</h1>} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 };
