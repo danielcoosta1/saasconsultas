5972201ed40a390667b52d0e8d1e9bba2a385e4d
Daniel Costa <daniel.dc697@gmail.com>
2025-06-08 12:49:26 -0300

    Criação da pagina ResetPassword

--- src/pages/ResetPassword/index.jsx
+++ src/pages/ResetPassword/index.jsx
@@ -2,54 +2,88 @@
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { toastErro, toastSucesso } from "../../utils/toast";
+import {
+  ButtonSubmit,
+  CampoInput,
+  ContainerPrincipal,
+  ContainerReset,
+  ContainerSuperior,
+  IconeSenha,
+  Input,
+  InputContainer,
+  LabelEstilizada,
+  Title,
+} from "./style";
+import logomarca from "../../assets/logomarca.png";
+import { IoEyeSharp } from "react-icons/io5";
+import { FaEyeSlash } from "react-icons/fa";
 
 const ResetPassword = () => {
-  const { token } = useParams(); // Obtém o token da URL
+  const { token } = useParams();
+  // Obtém o token da URL
+
   const navigate = useNavigate();
+
   const [senha, setSenha] = useState("");
+  const [erro, setErro] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  if (!senha) {
-    toastErro("Digite uma nova senha.");
-    return;
-  }
-
-  try {
-    const API_URL = import.meta.env.VITE_API_URL;
-    const response = await fetch(`${API_URL}/reset-password`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`, // Envia o token no cabeçalho
-      },
-      body: JSON.stringify({ senha }),
-    });
-
-    if (!response.ok) {
-      throw new Error("Erro ao redefinir senha.");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!senha) {
+      toastErro("Digite uma nova senha.");
+      return;
     }
 
-    toastSucesso("Senha alterada com sucesso!");
-    navigate("/login");
-  } catch (error) {
-    toastErro(error.message);
-  }
-};
+    try {
+      const API_URL = import.meta.env.VITE_API_URL;
+      const response = await fetch(`${API_URL}/reset-password`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`, // Envia o token no cabeçalho
+        },
+        body: JSON.stringify({ senha }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao redefinir senha.");
+      }
+
+      toastSucesso("Senha alterada com sucesso!");
+      navigate("/login");
+    } catch (error) {
+      setErro(error);
+      toastErro(error.message);
+    }
+  };
 
   return (
-    <div>
-      <h2>Redefinir Senha</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="password"
-          placeholder="Digite sua nova senha"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-        />
-        <button type="submit">Alterar Senha</button>
-      </form>
-    </div>
+    <ContainerPrincipal>
+      <ContainerReset onSubmit={handleSubmit}>
+        <ContainerSuperior>
+          <img src={logomarca} />
+          <Title>Reset password</Title>
+        </ContainerSuperior>
+        <CampoInput>
+          <LabelEstilizada>Password</LabelEstilizada>
+          <InputContainer>
+            <Input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              placeholder="**********"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+            />
+            <IconeSenha onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? <IoEyeSharp /> : <FaEyeSlash />}
+            </IconeSenha>
+          </InputContainer>
+          {erro && <p style={{ color: "red" }}>{erro}</p>}
+          <ButtonSubmit type="submit">Reset your password</ButtonSubmit>
+        </CampoInput>
+      </ContainerReset>
+    </ContainerPrincipal>
   );
 };
 
---
+++ src/pages/ResetPassword/style.js
@@ -0,0 +1,117 @@
+import styled from "styled-components";
+
+export const ContainerPrincipal = styled.section`
+  display: flex;
+  margin: 0 auto;
+  max-width: 75rem;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+
+  gap: 10rem;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const ContainerReset = styled.form`
+  background: #fff;
+  padding: ${({ theme }) => theme.spacing.large};
+  border-radius: 8px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  max-width: 700px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.xxlarge};
+`;
+export const ContainerSuperior = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    max-width: 92px;
+  }
+`;
+export const Title = styled.h2`
+  font-family: ${({ theme }) => theme.fonts.heading};
+  color: ${({ theme }) => theme.colors.text};
+  text-align: center;
+`;
+
+export const CampoInput = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+
+  justify-content: center;
+  gap: 0.5em;
+  padding: 0 2rem;
+`;
+
+export const LabelEstilizada = styled.label`
+  font-size: 0.75rem;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: ${({ theme }) => theme.spacing.medium};
+  padding-right: 40px; /* Espaço para o ícone */
+  border: none;
+  background-color: ${({ theme }) => theme.colors.inputs};
+  border-radius: 4px;
+  font-size: 14px;
+`;
+
+export const InputContainer = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+export const IconeSenha = styled.button`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  background: none;
+  border: none;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+
+  svg {
+    font-size: 20px;
+    color: ${({ theme }) => theme.colors.text};
+  }
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+export const ButtonSubmit = styled.button`
+  padding: ${({ theme }) => theme.spacing.medium};
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: #fff;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  font-size: 14px;
+  transition: background-color 0.3s ease;
+  margin-top: 2rem;
+  
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondary};
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+`;
+
+
